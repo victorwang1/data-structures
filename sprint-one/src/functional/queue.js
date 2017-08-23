0ff252b2709e9db31b542b824e5aebafed78555b
Aileen Fan <aileen.z.fan@gmail.com>
2017-08-23 14:36:40 -0700

    functional completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count++;
+    someInstance[count] = value;
   };
 
   someInstance.dequeue = function() {
+    var value = someInstance[1];
+    delete someInstance[1];
+    for (var i in someInstance) {
+        someInstance[count - 1] = someInstance[count];
+    }
+    delete someInstance[count];
+    count--;
+    return value;
   };
 
   someInstance.size = function() {
+    if (count < 0){
+      count = 0; 
+    }
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count++;
+    someInstance[count] = value;
   };
 
   someInstance.pop = function() {
+    var value = someInstance[count];
+    delete someInstance[count];
+    count--
+    return value;
+    
   };
 
   someInstance.size = function() {
+    if(count < 0) {
+      count = 0;
+    }
+    return count;
   };
 
   return someInstance;
