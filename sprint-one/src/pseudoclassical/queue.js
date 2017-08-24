203b2f5aec5d7b95250f25d7bee31d8b525d32d1
victorwang1 <victor.wang@me.com>
2017-08-23 20:18:30 -0700

    pass all tests for pseudoclassical functions

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this[this.count] = value;
+  this.count++;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.count > 0) {
+    var value = this[0];
+    for (var i = 0; i < this.count; i++) {
+      this[i] = this[i + 1];
+    }
+    delete this[this.count - 1];
+    this.count--;
+  }
+  return value;
+};
+
+Queue.prototype.size = function() {
+  return this.count;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0; 
 };
 
+Stack.prototype.push = function(value) {
+  this[this.count] = value;
+  this.count++;
+}; 
 
+
+Stack.prototype.pop = function() {
+  if (this.count > 0) {
+    var value = this[this.count - 1];
+    delete this[this.count - 1];
+    this.count--; 
+  }
+  return value;
+}; 
+
+Stack.prototype.size = function() {
+  return this.count;
+}; 
