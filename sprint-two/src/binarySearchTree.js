c7e7d603ff7053907e85a442754d84bdcfc26d66
victorwang1 <victor.wang@me.com>
2017-08-24 20:46:15 -0700

    passed all tests for tree and binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,44 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  var direction = this.getDirection(value);
+  if (this[direction] === null) {
+    this[direction] = new BinarySearchTree(value);
+  } else {
+    this[direction].insert(value);
+  }
+};
 
+BinarySearchTree.prototype.contains = function(value) {
+  var direction = this.getDirection(value);
+  if (value === this.value) {
+    return true;
+  }
+  if (this[direction] === null) {
+    return false;
+  }
+  return this[direction].contains(value);
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+  var output = [];
+  output.push(callback(this.value));
+  if (this.left !== null) {
+    output = output.concat(this.left.depthFirstLog(callback));
+  }
+  if (this.right !== null) {
+    output = output.concat(this.right.depthFirstLog(callback));
+  }
+  return output;
+};
+
+BinarySearchTree.prototype.getDirection = function(value) {
+  return value > this.value ? 'right' : 'left';
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,22 +1,29 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; 
+  _.extend(newTree, treeMethods);
+  
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  for (var child of this.children) {
+    if (child.contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
