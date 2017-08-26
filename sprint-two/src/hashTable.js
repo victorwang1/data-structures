b90f8d9e6e396e63ea66b44d77bb5e9d5e35f830
victorwang1 <victor.wang@me.com>
2017-08-25 19:49:37 -0700

    pass all minimum requirements for hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,13 +3,19 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-
+  this.count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
+  
+  this.count++;
+  if (this.count > 0.75 * this._limit) {
+    this.storage.resize(true);
+  }
+  
+  //*************DON'T TOUCH IT!!!**************
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-
   if (!bucket) {
     this._storage.set(index, []);
     bucket = this._storage.get(index);
@@ -25,7 +31,7 @@ HashTable.prototype.insert = function(k, v) {
   if (!found) { 
     bucket.push([k, v]);
   }
-  
+  //*************DON'T TOUCH IT!!!**************
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -40,6 +46,12 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
+  this.count--;
+  if (this.count > 8 && this.count < 0.25 * this._limit) {
+    this.storage.resize(false);
+  }
+  
+  //*************DON'T TOUCH IT!!!**************
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   for (var i = 0; i < bucket.length; i++) {
@@ -47,9 +59,22 @@ HashTable.prototype.remove = function(k) {
       bucket.splice(i, 1);
     }
   }
+  //*************DON'T TOUCH IT!!!**************
 };
 
+HashTable.prototype.resize = function(expand) {
+  var factor = expand ? 2 : 0.5;
+  this._limit = factor * this._limit;
 
+  var newTable = new LimitedArray(this._limit);
+  
+  this._storage.each(function(bucket, index, array) {
+    for (var element in bucket) {
+      newTable.insert(element[0], element[1]);
+    }
+  });
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
